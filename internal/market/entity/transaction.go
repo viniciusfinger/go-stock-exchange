08a7c3a4085512d731d1fc4b755cd7c0b1afeba3
Vinicius Finger <fingervinicius@icloud.com>
2023-06-19 22:09:58 -0300

    creating entities and market algorythm

---
+++ internal/market/entity/transaction.go
@@ -0,0 +1,28 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
+	ID           string
+	SellingOrder *Order
+	BuyingOrder  *Order
+	Price        float64
+	Total        float64
+	DateTime     time.Time
+}
+
+func NewTransaction(sellingOrder *Order, buyingOrder *Order, shares int, price float64) *Transaction {
+	total := float64(shares) * price
+
+	return &Transaction{
+		ID:           uuid.New().String(),
+		SellingOrder: sellingOrder,
+		BuyingOrder:  buyingOrder,
+		Total:        total,
+		Price:        price,
+	}
+}
